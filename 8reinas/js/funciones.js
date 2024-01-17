133a3581fe40ea0c6907c422619af006d4a8b1fe
Carlos <131988778+JCLV2002@users.noreply.github.com>
2024-01-17 12:06:05 -0700

    Formato
    formato al documento

--- 8reinas/js/funciones.js
+++ 8reinas/js/funciones.js
@@ -2,24 +2,24 @@ var reinasPorColocar = 8;
 var reinascolocadas = 0;
 
 
-function colocarReina(celda){
+function colocarReina(celda) {
    /* alert("le dieron click a la celda" + celda);*/
    if (window.getComputedStyle(celda).backgroundImage == "none") {
-      if (reinascolocadas <8 ){
-    celda.style= " background-image: url(./img/reina.png); background-size: cover;";
-    var renglon = celda.parentElement.rowIndex;
-    var columna = celda.cellIndex;
-    alert(renglon+ "" + columna);
-    reinasPorColocar --;
-    reinascolocadas ++;
+      if (reinascolocadas < 8) {
+         celda.style = " background-image: url(./img/reina.png); background-size: cover;";
+         var renglon = celda.parentElement.rowIndex;
+         var columna = celda.cellIndex;
+         alert(renglon + "" + columna);
+         reinasPorColocar--;
+         reinascolocadas++;
       }
    } else {
-    celda.style= " background-image: none;";
-    reinasPorColocar ++;
-    reinascolocadas --;
+      celda.style = " background-image: none;";
+      reinasPorColocar++;
+      reinascolocadas--;
    }
 
-   document.getElementById("RC").innerHTML="Reinas por colocar"+reinasPorColocar;
+   document.getElementById("RC").innerHTML = "Reinas por colocar" + reinasPorColocar;
 
-   document.getElementById("RCS").innerHTML="Reinas colocadas:"+reinascolocadas;
+   document.getElementById("RCS").innerHTML = "Reinas colocadas:" + reinascolocadas;
 }
